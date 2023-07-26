c45dc1b3f4079eec1cb5896635f752cac7ba7f89
BasharElmetwaliahmed <113910248+BasharElmetwaliahmed@users.noreply.github.com>
2023-07-26 07:47:55 +0300

    update landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import landingimg from '../assets/images/illustration-intro.svg'
 function Landing() {
   return (
     <div>
-        <div className='container  flex  pt-[60px] relative lg:justify-between gap:10 flex-col-reverse lg:flex-row  pt-[130px]  '>
+        <div className='container  flex  relative lg:justify-between gap:10 flex-col-reverse lg:flex-row  pt-[150px]  '>
             <div className='lg:w-[49%] w-[100%]  flex flex-col justify-center text-center items-center lg:items-start lg:text-start   gap-9'>
                 <h1 className='text-bluedark font-bold text-[45px]'>Bring everyone <br/> together to build<br/> better products.</h1>
                 <p className='text-bluegrayish w-full lg:w-[49%]'>  Manage makes it simple for software teams to plan day-to-day 
